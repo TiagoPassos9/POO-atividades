354cfebbc51974eaf58d10a97a876b82cd9e237d
TiagoPassos9 <102630544+TiagoPassos9@users.noreply.github.com>
2023-05-02 19:12:13 -0300

    Atividade quadrado

---
+++ Quadrado.java
@@ -0,0 +1,39 @@
+public class Quadrado {
+    private Cor cor;
+    private String tamanho;
+
+
+    public Quadrado(Cor cor, String tamanho) {
+        this.cor = cor;
+        this.tamanho = tamanho;
+    }
+
+
+    public Cor getColor() {
+        return cor;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    private void setTamanho(String tamanho) {
+        if (Integer.parseInt(tamanho) > 0) {
+            this.tamanho = tamanho;
+        } else {
+            System.out.println("O tamanho precisa ser maior que 0!");
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Quadrado cor: " + cor + ", tamanho: " + tamanho;
+    }
+
+    public static void main(String[] args) {
+        Quadrado quadrado = new Quadrado(Cor.AMARELO, "Grande");
+        System.out.println(quadrado.toString());
+    }
+
+}
